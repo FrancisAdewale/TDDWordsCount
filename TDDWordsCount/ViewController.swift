03e48bf5bcba26128f77364d5e1756d731419e1b
Francis Adewale <71708756+FrancisAdewale@users.noreply.github.com>
2021-05-05 12:34:37 +0100

    first commit

---
+++ TDDWordsCount/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  TDDWordsCount
+//
+//  Created by Francis Adewale on 05/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var string: String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func countWords(in string: String) -> Dictionary<String,Int> {
+        
+        
+        let dataSet = Dictionary<String, Int>()
+        return dataSet
+        
+    }
+
+}
+
---
+++ TDDWordsCountTests/TDDWordsCountTests.swift
@@ -0,0 +1,59 @@
+//
+//  TDDWordsCountTests.swift
+//  TDDWordsCountTests
+//
+//  Created by Francis Adewale on 05/05/2021.
+//
+
+import XCTest
+@testable import TDDWordsCount
+
+class TDDWordsCountTests: XCTestCase {
+    
+    var sut: ViewController!
+
+    override func setUp()  {
+        super.setUp()
+        sut = ViewController()
+    }
+
+    
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+
+    func test_returnsEmptyDataStruct()  {
+        
+        //given
+        
+        sut.string = ""
+        
+        //when
+        
+        let emptyData = sut.countWords(in: sut.string!)
+        
+        //then
+        
+        XCTAssertTrue(emptyData.count == 0)
+        
+        
+    }
+    
+    func test_returnStructWithData() {
+        
+        
+        //given
+        
+        
+        
+        //when
+        
+        //then
+        
+    }
+    
+
+}
